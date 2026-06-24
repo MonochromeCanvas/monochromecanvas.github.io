3d6e3e1fe7882153484cc33398851e28ff13a78c
MonochromeCanvas <joellezellman@gmail.com>
2026-06-24 15:44:08 -0400

    Connect community canvas to Supabase

--- community-canvas/config.js
+++ community-canvas/config.js
@@ -1,6 +1,6 @@
 window.COMMUNITY_CANVAS_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://marobxcafpzqrriipjdw.supabase.co",
+  supabaseAnonKey: "sb_publishable_Qe7VGFqtCZeBDwtbn3LfDA_t7CXfo3_",
   artworkBucket: "community-canvas-artwork",
   adminEmail: "studio@monochromecanvas.com"
 };
